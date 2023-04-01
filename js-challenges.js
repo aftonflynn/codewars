5964fc7b9795846656f4a50d3a4e190888f4251b
Afton Flynn <aftonflynn@outlook.com>
2023-03-31 20:13:02 -0600

    Mar 31, 2023 daily challenge

--- js-challenges.js
+++ js-challenges.js
@@ -1,3 +1,25 @@
+// MAR 31, 2023 //
+/////// Disemvowel Trolls ///////
+/*
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+
+*/
+function disemvowel(str) {
+  const vowels = 'aeiouAEIOU'
+  return str.split("")
+    .filter((c) => !vowels.includes(c))
+    .join("")
+}
+
+
 // MAR 26, 2023 //
 /////// SpeedCode #3 × Fun with ES6 Classes #5 - Dogs and Classes ///////
 /*
