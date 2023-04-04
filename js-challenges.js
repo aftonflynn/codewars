eb09659dc5ad09b4a211fb4f2a9b0900e2d9ee4e
Afton Flynn <aftonflynn@outlook.com>
2023-04-03 20:42:43 -0600

    Apr 3, 2023 daily challenge

--- js-challenges.js
+++ js-challenges.js
@@ -1,3 +1,25 @@
+// APR 3, 2023 //
+/////// Detect Pangram ///////
+/*
+A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+*/
+
+function isPangram(string) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let arr = [];
+  string.toLowerCase().split("")
+    .forEach((letter, i) => {
+      if (alphabet.includes(letter) && !arr.includes(letter)) {
+        arr.push(letter);
+      }
+    });
+
+  return (arr.length === 26) ? true : false;
+}
+
+
 // MAR 31, 2023 //
 /////// Disemvowel Trolls ///////
 /*
